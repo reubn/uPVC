8a0d89c6154bd69e0deeb3b88a31a0d39181113b
reubn <4957964+reubn@users.noreply.github.com>
2020-11-24 21:54:04 +0000

    🔥 Frame Skip Option

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ macOS tool to extract frames from video. Supports alpha channel. `16-bit RGBA PN
 ## Usage
 
 ```bash
-./uPVC ./videofile.mov
+./uPVC ./videofile.mov 3 # output every 3rd frame
 
 => ./videofile/frame-0.png.....frame-1650.png
 ```
--- main.swift
+++ main.swift
@@ -15,6 +15,8 @@ var stderr = StandardErrorOutputStream()
 if(arguments.count == 0) {print("need a file", to: &stderr); exit(1)}
 
 let videoPath = arguments[0]
+let frameSkip = arguments.count > 1 ? Int(arguments[1]) : nil
+
 let videoURL = URL(fileURLWithPath: videoPath)
 
 let asset = AVAsset(url: videoURL)
@@ -36,32 +38,33 @@ reader!.startReading()
 var frameIndex = 0
 
 while let sampleBuffer = trackReaderOutput.copyNextSampleBuffer() {
-    if  let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
-        let ciimage: CIImage = CIImage(cvPixelBuffer: imageBuffer)
-        
-        if let colorSpace = CGColorSpace(name: CGColorSpace.sRGB) {
-          do {
-            let format = CIFormat.RGBA16 // 16-bit RGBA
-            let quality = 1.0 // 1.0 = lossless
-            
-            let outFolderURL = videoURL.deletingPathExtension()
-            let outURL = outFolderURL.appendingPathComponent("frame-\(frameIndex).png")
-                
-            let context = CIContext()
-                
-            try FileManager.default.createDirectory(at: outFolderURL, withIntermediateDirectories: true, attributes: nil)
-            try context.writePNGRepresentation(of: ciimage, to: outURL, format: format, colorSpace: colorSpace, options: [kCGImageDestinationLossyCompressionQuality as CIImageRepresentationOption: quality])
-          } catch {
-            print(error.localizedDescription, to: &stderr)
-          }
+  if((frameSkip != nil) && ((frameIndex % frameSkip!) == 0)) {
+    if let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+      let ciimage: CIImage = CIImage(cvPixelBuffer: imageBuffer)
+      
+      if let colorSpace = CGColorSpace(name: CGColorSpace.sRGB) {
+        do {
+          let format = CIFormat.RGBA16 // 16-bit RGBA
+          let quality = 1.0 // 1.0 = lossless
+          
+          let outFolderURL = videoURL.deletingPathExtension()
+          let outURL = outFolderURL.appendingPathComponent("frame-\(frameIndex).png")
+          
+          let context = CIContext()
+          
+          try FileManager.default.createDirectory(at: outFolderURL, withIntermediateDirectories: true, attributes: nil)
+          try context.writePNGRepresentation(of: ciimage, to: outURL, format: format, colorSpace: colorSpace, options: [kCGImageDestinationLossyCompressionQuality as CIImageRepresentationOption: quality])
+        } catch {
+          print(error.localizedDescription, to: &stderr)
         }
-        
-        frameIndex += 1
+      }
     }
+  }
+  frameIndex += 1
 }
 
 final class StandardErrorOutputStream: TextOutputStream {
-    func write(_ string: String) {
-        FileHandle.standardError.write(Data(string.utf8))
-    }
+  func write(_ string: String) {
+    FileHandle.standardError.write(Data(string.utf8))
+  }
 }
